2d2f2fac246dbac796eeed42f8bcdb826b81155c
Roberts Kalnins <rkalnins@umich.edu>
2021-10-24 13:18:03 -0400

    Add reflectance messages

---
+++ src/subsys/reflectance_array_subsystem.c
@@ -0,0 +1,30 @@
+#include "reflectance_array_subsystem.h"
+
+#include <os.h>
+#include <stm32l4xx_hal.h>
+
+#include "app_defs.h"
+#include "stm/stm32l4xx.h"
+
+extern void REFARR_Init()
+{
+}
+
+extern void ReflectanceArrayEventHandler(Message_t* msg)
+{
+    if(msg->id == REFARR_CALIBRATE_MSG_ID)
+    {
+    }
+    else if(msg->id == REFARR_ON_MSG_ID)
+    {
+    }
+    else if(msg->id == REFARR_OFF_MSG_ID)
+    {
+    }
+    else if(msg->id == REFARR_START_READ_MSG_ID)
+    {
+    }
+    else if(msg->id == REFARR_STOP_READ_MSG_ID)
+    {
+    }
+}
