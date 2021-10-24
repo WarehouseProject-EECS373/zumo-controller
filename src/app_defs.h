2d79eee48db92f8a04980c5dfd4c889bff5c171f
Roberts Kalnins <rkalnins@umich.edu>
2021-10-24 12:51:13 -0400

    Add more drive configuration

--- src/app_defs.h
+++ src/app_defs.h
@@ -16,18 +16,33 @@ typedef struct DriveControlMessage_s
     float actual;
 } DriveControlMessage_t;
 
+typedef struct DriveSetpointMessage_s
+{
+    Message_t base;
+    float setpoint;
+    uint32_t drive_mode;
+} DriveSetpointMessage_t;
+
+typedef struct DriveBaseVelocityMessage_s
+{
+    Message_t base;
+    float base_velocity;
+} DriveBaseVelocityMessage_t;
+
 //*****************************************************************/
 // MESSAGE IDs
 //*****************************************************************/
 
-#define DATA_MSG_ID      0x0
-#define HEARTBEAT_MSG_ID 0x1
+#define DATA_MSG_ID        0x0
+#define SENSOR_READ_MSG_ID 0x1
+#define HEARTBEAT_MSG_ID   0x2
 
 #define DRIVE_CTL_IN_MSG_ID         0x10
 #define DRIVE_DISABLE_MSG_ID        0x11
 #define DRIVE_ENABLE_MSG_ID         0x12
 #define DRIVE_TIMED_ACTIVITY_MSG_ID 0x13
 #define DRIVE_BASE_VELOCITY_MSG_ID  0x14
+#define DRIVE_SETPOINT_MSG_ID       0x14
 
 #define PUSH_BUTTON_PRESSED_MSG_ID 0x21
 
@@ -35,8 +50,10 @@ typedef struct DriveControlMessage_s
 // Active Object Extern Declarations and Configuration
 //*****************************************************************/
 
-#define HEARTBEAT_QUEUE_SIZE 2
+#define HEARTBEAT_QUEUE_SIZE 1
+#define DRIVE_SS_QUEUE_SIZE  16
 
 ACTIVE_OBJECT_EXTERN(heartbeat_ao, HEARTBEAT_QUEUE_SIZE)
+ACTIVE_OBJECT_EXTERN(drive_ss_ao, DRIVE_SS_QUEUE_SIZE)
 
 #endif
--- src/main.c
+++ src/main.c
@@ -12,13 +12,20 @@
 #include "subsys/drive_subsystem.h"
 #include "subsys/input_ctl_subsystem.h"
 
-#define HEARTBEAT_PERIOD 500
+//*****************************************************************/
+// Periodic Timing Definitions
+//*****************************************************************/
+
+#define HEARTBEAT_PERIOD               500
+#define IR_SENSOR_READ_PERIOD          3
+#define DRIVE_SS_TIMED_ACTIVITY_PERIOD 5
 
 //*****************************************************************/
 // Active Object Declarations and Configuration
 //*****************************************************************/
 
 ACTIVE_OBJECT_DECL(heartbeat_ao, HEARTBEAT_QUEUE_SIZE)
+ACTIVE_OBJECT_DECL(drive_ss_ao, DRIVE_SS_QUEUE_SIZE)
 
 //*****************************************************************/
 // Application and local declarations
@@ -26,9 +33,25 @@ ACTIVE_OBJECT_DECL(heartbeat_ao, HEARTBEAT_QUEUE_SIZE)
 
 extern OS_t os;
 
+//*****************************************************************/
+// Timed Events and their messages
+//*****************************************************************/
+
 static TimedEventSimple_t hb_event;
 static Message_t hb_msg = {.id = HEARTBEAT_MSG_ID, .msg_size = sizeof(Message_t)};
 
+static TimedEventSimple_t ir_sensor_read_event;
+static Message_t ir_sensor_read_msg = {.id = SENSOR_READ_MSG_ID, .msg_size = sizeof(Message_t)};
+
+static TimedEventSimple_t drive_ss_ctl_loop_event;
+static Message_t drive_ss_ctl_loop_msg = {.id = DRIVE_TIMED_ACTIVITY_MSG_ID,
+                                          .msg_size = sizeof(Message_t)};
+
+/**
+ * @brief Simple LED heartbeat so we know everything is ok. Tie this into a watchdog eventually.
+ *
+ * @param msg
+ */
 void HeartbeatHandler(Message_t* msg)
 {
     if(HEARTBEAT_MSG_ID == msg->id)
@@ -67,15 +90,19 @@ int main()
     ITCTL_Init();
 
     // start subsystems
-    Drive_SetOutputPercent(0.0, 0.0);
 
     // initialize all active objects
     AO_INIT(heartbeat_ao, 6, HeartbeatHandler, HEARTBEAT_QUEUE_SIZE)
+    AO_INIT(drive_ss_ao, 2, DriveEventHandler, DRIVE_SS_QUEUE_SIZE)
 
     // create and schedule timed events.
     TimedEventSimpleCreate(&hb_event, &heartbeat_ao, &hb_msg, HEARTBEAT_PERIOD,
                            TIMED_EVENT_PERIODIC_TYPE);
     SchedulerAddTimedEvent(&hb_event);
+
+    TimedEventSimpleCreate(&drive_ss_ctl_loop_event, &drive_ss_ao, &drive_ss_ctl_loop_msg,
+                           DRIVE_SS_TIMED_ACTIVITY_PERIOD, TIMED_EVENT_PERIODIC_TYPE);
+    SchedulerAddTimedEvent(&drive_ss_ctl_loop_event);
 
     // initialze kernel
     OSCallbacksCfg_t os_callback_cfg = {
